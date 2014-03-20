011e5f872bc2369d16b21f68f940c07992db76a5
josephhughes <hughes.joseph@gmail.com>
2014-03-20 11:54:06 +0000

    addition of singletons sorting

--- SelectSeq.pl
+++ SelectSeq.pl
@@ -9,7 +9,7 @@ use Getopt::Long;
 use Bio::SeqIO;                    
  
 
-my ($infasta,$outfile,$fid,$fdescr,$rdm,$help,$mid,$idfile);
+my ($infasta,$outfile,$fid,$fdescr,$rdm,$help,$mid,$idfile,$singletons,$frequency);
 &GetOptions(
 	    'in:s'      => \$infasta,#fastafile
 	    'out:s'   => \$outfile,#output fasta file
@@ -18,11 +18,12 @@ my ($infasta,$outfile,$fid,$fdescr,$rdm,$help,$mid,$idfile);
 	    'idfile:s' => \$idfile, #text file with mutliple ids to pull out
 	    "fdescr:s"  => \$fdescr,  # find descr
 	    "rdm"  => \$rdm,  # randomly select a fasta sequence
+	    "singletons" => \$singletons, # split file into singletons and non-singletons
+	    "freq:s"  => \$frequency, #frequency a sequence needs to be found at
 	    "help"  => \$help,  # provides help with usage
            );
-
 if (($help)&&!($help)||!($infasta)||!($outfile)){
- print "Usage : Consensus.pl <list of arguments>\n";
+ print "Usage : SelectSeq.pl <list of arguments>\n";
  print " -in <txt> - the input fasta file\n";
  print " -out <txt> - the name of your output fasta file\n";
  print " -fid <txt> - the id of the fasta file you want to match - exact match\n";
@@ -30,6 +31,7 @@ if (($help)&&!($help)||!($infasta)||!($outfile)){
  print " -idfile <txt> - a file with multiple ids you want to match pull out\n";
  print " -fdescr <txt> - the description of a fasta sequence you want to match - partial match\n";
  print " -rdm - randomly select a sequence from a fasta file\n";
+ print " -singletons - split file into singletons and no singletons fasta\n";
  print " -help        - Get this help\n";
  exit();
  }
@@ -46,16 +48,19 @@ while (<IDS>){
 }
 my $in  = Bio::SeqIO->new(-file => "$infasta" ,
                          -format => 'fasta');
-my $out = Bio::SeqIO->new(-file => ">$outfile" , '-format' => 'fasta');
+my $outid = Bio::SeqIO->new(-file => ">In$outfile" , '-format' => 'fasta');
+my $out = Bio::SeqIO->new(-file => ">Not$outfile" , '-format' => 'fasta');
 while (my $seq = $in->next_seq()){
   my $id=$seq->id;
-  foreach my $listid (keys %list){
-    if ($id eq $listid){
-     $out->write_seq($seq);
+ ## foreach my $listid (keys %list){
+    if ($list{$id}){
+     $outid->write_seq($seq);
      $hits++;
-     delete $list{$listid};
+     delete $list{$id};
+    }else{
+      $out->write_seq($seq);
     }
-  }
+ ## }
 }
 my @nomatches=keys %list;
 print "There are $hits matches from the $idcnt ids\n";
@@ -116,4 +121,67 @@ my $out = Bio::SeqIO->new(-file => ">$outfile" , '-format' => 'fasta');
   my $rdmseq = $seqs[$index];
   print "$rdmseq\n";
   $out->write_seq($rdmseq);
+}elsif($singletons){
+my %seen;
+my $in  = Bio::SeqIO->new(-file => "$infasta" ,
+                         -format => 'fasta');
+my $nosingletons = Bio::SeqIO->new(-file => ">$outfile.nosingletons.fa" , '-format' => 'fasta');
+my $singletons = Bio::SeqIO->new(-file => ">$outfile.singletons.fa" , '-format' => 'fasta');
+
+  while (my $seq = $in->next_seq()){
+    my $id=$seq->id;
+    my $seqstr=$seq->seq;
+    $seen{$seqstr}{$id}++;
+  }
+  foreach my $seqstr (keys %seen){
+    my $nbseqs=keys %{$seen{$seqstr}};
+    print "$nbseqs $seqstr\n";
+    if ($nbseqs>1){
+      foreach my $id (keys %{$seen{$seqstr}}){
+         my $seq_obj = Bio::Seq->new(-seq => $seqstr, 
+                          -display_id => $id);#-desc => $nbseqs, 
+        $nosingletons->write_seq($seq_obj);
+      }
+    }else{
+      foreach my $id (keys %{$seen{$seqstr}}){
+         my $seq_obj = Bio::Seq->new(-seq => $seqstr,
+                          -display_id => $id);# -desc => $nbseqs, 
+        $singletons->write_seq($seq_obj);
+      }
+    }
+  }
+
+}elsif($frequency){
+my %seen;
+my $in  = Bio::SeqIO->new(-file => "$infasta" ,
+                         -format => 'fasta');
+my $above = Bio::SeqIO->new(-file => ">$outfile.above$frequency.fa" , '-format' => 'fasta');
+my $below = Bio::SeqIO->new(-file => ">$outfile.below$frequency.fa" , '-format' => 'fasta');
+my $totalseq=0;
+  while (my $seq = $in->next_seq()){
+    my $id=$seq->id;
+    my $seqstr=$seq->seq;
+    $seen{$seqstr}{$id}++;
+    $totalseq++;
+  }
+  foreach my $seqstr (keys %seen){
+    my $nbseqs=keys %{$seen{$seqstr}};
+    my $seqfreq=$nbseqs/$totalseq;
+    print "$nbseqs $seqfreq $seqstr\n";
+    if ($seqfreq>$frequency){
+      foreach my $id (keys %{$seen{$seqstr}}){
+         my $seq_obj = Bio::Seq->new(-seq => $seqstr, 
+                          -display_id => $id);#-desc => $nbseqs, 
+        $above->write_seq($seq_obj);
+      }
+    }elsif ($seqfreq<=$frequency){
+      foreach my $id (keys %{$seen{$seqstr}}){
+         my $seq_obj = Bio::Seq->new(-seq => $seqstr,
+                          -display_id => $id);# -desc => $nbseqs, 
+        $below->write_seq($seq_obj);
+      }
+    }
+  }
+
 }
+
