50331911bf347098e4e841e95bfcc611f7732679
Joseph Hughes <joseph.hughes@glasgow.ac.uk>
2016-05-13 16:33:16 +0100

    adding option to have gi in identifier

--- gb2fa.pl
+++ gb2fa.pl
@@ -7,10 +7,11 @@ use strict;
 use Getopt::Long; 
 use Bio::SeqIO;
 
-my ($infile,$outfile);
+my ($infile,$outfile,$gi);
 &GetOptions(
 	    'in:s'      => \$infile,#genbank input 
 	    'out:s'    =>\$outfile,#in fasta
+	    'gi' =>\$gi,#specify that you want the gi|number|gb|accession identifier
            );
 
 my $seq_in  = Bio::SeqIO->new(-file => "$infile" , '-format' => 'genbank');
@@ -20,6 +21,17 @@ my $seq_out = Bio::SeqIO->new( -file   => ">$outfile",
                              );
 
 # write each entry in the input file to the output file
-while (my $inseq = $seq_in->next_seq) {
-   $seq_out->write_seq($inseq);
+if ($gi){
+  while (my $inseq = $seq_in->next_seq) {
+    my $gi = $inseq->primary_id();
+    my $descr=$inseq->desc();
+    my $acc = $inseq->accession_number();
+    my $new_id="gi|$gi|gb|$acc";
+    $inseq->id($new_id);
+    $seq_out->write_seq($inseq);
+  }
+}else{
+  while (my $inseq = $seq_in->next_seq) {
+    $seq_out->write_seq($inseq);
+  }
 }
