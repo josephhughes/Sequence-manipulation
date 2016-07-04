c26cec4fe17e6942f8d4d52dcca8846aa5916e4f
Joseph Hughes <joseph.hughes@glasgow.ac.uk>
2016-07-04 16:47:47 +0100

    adding -r option to replace gaps with Ns

--- RemoveGaps.pl
+++ RemoveGaps.pl
@@ -7,10 +7,11 @@ use strict;
 use Getopt::Long; 
 use Bio::SeqIO;
 
-my ($in,$out,$help);
+my ($in,$out,$help,$r);
 &GetOptions(
 	    'in:s'      => \$in,#a multifasta sequence file 
 	    'out:s'      => \$out,#output file 
+	    'r'     =>  \$r,
 	    "help"  => \$help,  # provides help with usage
            );
 
@@ -18,19 +19,26 @@ if (($help)&&!($help)||!($in)){
  print "Usage : RemoveGaps.pl <list of arguments>\n";
  print " -in <txt> - fasta files\n";
  print " -out <txt> - output fasta files\n";
+ print "-r  - replace gaps wirh Ns\n";
  print " -help        - Get this help\n";
  exit();
  }
-
 
 my $seqio_obj = Bio::SeqIO->new(-file => "$in", -format => 'fasta' );
 my $seq_out;
 if ($out){
  $seq_out = Bio::SeqIO->new(-file => ">$out", -format => 'fasta' );
 }
+
 while( my $seq = $seqio_obj->next_seq ) {
   my $seq_str=$seq->seq;
-  $seq_str=~s/\-//g;
-  $seq->seq($seq_str);
-  $seq_out->write_seq($seq);
+  if ($r){
+    $seq_str=~s/\-/N/g;
+    $seq->seq($seq_str);
+    $seq_out->write_seq($seq);
+  }else{
+    $seq_str=~s/\-//g;
+    $seq->seq($seq_str);
+    $seq_out->write_seq($seq);
+  }
 }
